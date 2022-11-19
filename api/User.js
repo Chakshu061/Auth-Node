6a57bcf5ce33883a399f4d913597a98f47922a38
Chakshu Shaktawat <93027256+Chakshu061@users.noreply.github.com>
2022-11-19 21:37:26 +0530

    Update User.js

--- api/User.js
+++ api/User.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const { find } = require('../models/User');
 const router = express.Router();
+
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 //Signup 
 router.post('/signup', (req, res) => {
@@ -9,18 +13,51 @@ router.post('/signup', (req, res) => {
     password = password.trim();
     dateOfBirth = dateOfBirth.trim();
 
-    if(name == "" || email == "" || password == "" || dateOfBirth){
+    if (name == "" || email == "" || password == "" || dateOfBirth){
         res.json({
             status: "FAILED",
-            message: "Empty input fields!";
+            message: "Empty input fields!"
         });
     } else if (!/^[a-zA-Z]*$/.test){
         res.json({
             status: "FAILED",
             message: "Invalid name entered"
         })
-    } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
-})
+    } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)){
+        res.json({
+            status: "FAILED",
+            message: "Invalid email entered"
+        })
+    } else if (!new Date(dateOfBirth).getTime()){
+        res.json({
+            status: "FAILED",
+            message: "Invalid date of birth entered"
+        })
+    } else if (password.length < 8){
+        res.json({ 
+            status: "FAILED",
+            message: "Password must be at least 8 characters"
+        })
+    } else {
+        User.find({email}).then(result => {
+            if (result.length) {
+                res.json({
+                    status: "FAILED",
+                    message: "User with the provided email id already exists"
+                })
+            } else {
+                    const saltRounds = 10;
+                    bcrypt.hash(password, saltRounds).then(hashedPassword)
+            }
+        }).catch(err => {
+            console.log(err);
+            res.json({
+                status: "FAILED",
+                message: "An error occured while checking for existing user"
+            })
+        })
+    }
+});
 
 router.post('/signin', (req, res) => {
 
